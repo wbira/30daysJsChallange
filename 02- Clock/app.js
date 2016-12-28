9589ba986c393743f257e906e86aa0a24e045c97
Waldemar Bira <waldek.bira@gmail.com>
2016-12-28 21:08:19 +0100

    Update degrees calculation

--- 02- Clock/app.js
+++ 02- Clock/app.js
@@ -1,9 +1,10 @@
 
 function setClockHands() {
-    var currentTime = new Date();
-    updateSecondHand(currentTime);
-    updateMinuteHand(currentTime);
-    updateHourHand(currentTime);
+    var currentTime = new Date(), seconds, mins;
+    
+    seconds = updateSecondHand(currentTime);
+    mins = updateMinuteHand(currentTime, seconds);
+    updateHourHand(currentTime, mins);
 }
 
 
@@ -12,18 +13,20 @@ function updateSecondHand(currentTime) {
         secondsDegree = (seconds / 60) * 360 + 90,
         secondHand = document.querySelector('.second-hand');
     rotateHand(secondHand, secondsDegree);
+    return seconds;
 }
 
-function updateMinuteHand(currentTime) {
+function updateMinuteHand(currentTime, seconds) {
     var min = currentTime.getMinutes(),
-        minDegree = (min / 60) * 360 + 90;
+        minDegree = (min / 60) * 360 + ((seconds / 60) * 6) + 90;
     minHand = document.querySelector('.min-hand');
     rotateHand(minHand, minDegree);
+    return min;
 }
 
-function updateHourHand(currentTime) {
+function updateHourHand(currentTime, mins) {
     var hour = currentTime.getHours(),
-        hourDegree = (hour / 12) * 360 + 90,
+        hourDegree = (hour / 12) * 360 + ((mins / 60) * 30) + 90,
         hourHand = document.querySelector('.hour-hand');
     rotateHand(hourHand, hourDegree);
 }
